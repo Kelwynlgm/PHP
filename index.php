288206dd387bfe67d4b0d23667fb5246d498a22f
kelwyn <kelwynlgm@hotmail.com>
2019-01-19 20:17:55 -0300

    teste 1

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Olá Mundo!</title>
+	<style type="text/css">
+		div{
+			width: 200px;
+			height: 50px;
+			margin: 0 auto;
+			background-color: rgba(0,0,255,0.7);
+			border: 2px solid black;
+			border-radius: 5px;
+			color: white;
+			padding: 10px;
+			margin-bottom: 10px;
+		}
+	</style>
+</head>
+<body>
+	<div>
+		<?php
+			echo "Olá Mundo!";
+		?>
+	</div>
+		<div>
+		<?php
+			echo "Olá Mundo!";
+		?>
+	</div>
+		<div>
+		<?php
+			echo "Olá Mundo!";
+		?>
+	</div>
+		<div>
+		<?php
+			echo "Olá Mundo!";
+		?>
+	</div>
+</body>
+</html>
